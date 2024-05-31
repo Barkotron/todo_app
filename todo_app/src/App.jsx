6e55cecfdad8beec12a2a77a78c1cc84f8a41e08
Jackson Barker <jcrb45@hotmail.com>
2024-05-31 09:32:00 -0500

    initial commit

---
+++ todo_app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Sidebar></Sidebar>
+    </>
+  )
+}
+
+export default App
